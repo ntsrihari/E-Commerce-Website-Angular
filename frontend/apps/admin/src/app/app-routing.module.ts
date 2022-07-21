35b401878f112aee69a29dfb63e2b41a6ebde586
Srihari <nts.srihari@gmail.com>
2022-07-21 11:57:44 +0530

    commit 2

--- frontend/apps/admin/src/app/app-routing.module.ts
+++ frontend/apps/admin/src/app/app-routing.module.ts
@@ -1,79 +1,80 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '@bluebits/users';
-import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
-import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { OrdersDetailComponent } from './pages/orders/orders-detail/orders-detail.component';
-import { OrdersListComponent } from './pages/orders/orders-list/orders-list.component';
-import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
-import { ProductsListComponent } from './pages/products/products-list/products-list.component';
-import { UsersFormComponent } from './pages/users/users-form/users-form.component';
-import { UsersListComponent } from './pages/users/users-list/users-list.component';
-import { ShellComponent } from './shared/shell/shell.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "@bluebits/users";
+import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
+import { CategoriesListComponent } from "./pages/categories/categories-list/categories-list.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { OrdersDetailComponent } from "./pages/orders/orders-detail/orders-detail.component";
+import { OrdersListComponent } from "./pages/orders/orders-list/orders-list.component";
+import { ProductsFormComponent } from "./pages/products/products-form/products-form.component";
+import { ProductsListComponent } from "./pages/products/products-list/products-list.component";
+import { UsersFormComponent } from "./pages/users/users-form/users-form.component";
+import { UsersListComponent } from "./pages/users/users-list/users-list.component";
+import { ShellComponent } from "./shared/shell/shell.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: ShellComponent,
     canActivate: [AuthGuard],
     children: [
       {
-        path: '',
-        component: DashboardComponent
+        path: "",
+        // component: DashboardComponent,
+        component: ProductsListComponent,
       },
       {
-        path: 'categories',
-        component: CategoriesListComponent
+        path: "categories",
+        component: CategoriesListComponent,
       },
       {
-        path: 'categories/form',
-        component: CategoriesFormComponent
+        path: "categories/form",
+        component: CategoriesFormComponent,
       },
       {
-        path: 'categories/form/:id',
-        component: CategoriesFormComponent
+        path: "categories/form/:id",
+        component: CategoriesFormComponent,
       },
       {
-        path: 'products',
-        component: ProductsListComponent
+        path: "products",
+        component: ProductsListComponent,
       },
       {
-        path: 'products/form',
-        component: ProductsFormComponent
+        path: "products/form",
+        component: ProductsFormComponent,
       },
       {
-        path: 'products/form/:id',
-        component: ProductsFormComponent
+        path: "products/form/:id",
+        component: ProductsFormComponent,
       },
       {
-        path: 'users',
-        component: UsersListComponent
+        path: "users",
+        component: UsersListComponent,
       },
       {
-        path: 'users/form',
-        component: UsersFormComponent
+        path: "users/form",
+        component: UsersFormComponent,
       },
       {
-        path: 'users/form/:id',
-        component: UsersFormComponent
+        path: "users/form/:id",
+        component: UsersFormComponent,
       },
       {
-        path: 'orders',
-        component: OrdersListComponent
+        path: "orders",
+        component: OrdersListComponent,
       },
       {
-        path: 'orders/:id',
-        component: OrdersDetailComponent
-      }
-    ]
-  }
+        path: "orders/:id",
+        component: OrdersDetailComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, { initialNavigation: "enabled" })],
   exports: [RouterModule],
   declarations: [],
-  providers: []
+  providers: [],
 })
 export class AppRoutingModule {}
